10deba56bc2ca4d74383625500dfda6d53cd8ebe
huangwei <huangwei06@58ganji.com>
2019-02-21 19:39:33 +0800

    setup

--- setup.sh
+++ setup.sh
@@ -5,10 +5,8 @@ HOST=$1
 USER_NAME=$2
 USER_PASS=$3
 
-curl  https://get.acme.sh | sh
-alias acme.sh=~/.acme.sh/acme.sh
 yum -y install socat
-
+curl  https://get.acme.sh | sh
 
 # 打开80和443端口
 firewall-cmd --zone=public --add-port=80/tcp --permanent
@@ -16,10 +14,10 @@ firewall-cmd --zone=public --add-port=443/tcp --permanent
 systemctl restart firewalld.service
 
 # 生成证书
-acme.sh  --issue -d $HOST --standalone
+bash ~/.acme.sh/acme.sh  --issue -d $HOST --standalone
 
 # 证书更新时自动copy证书
-acme.sh  --installcert  -d $HOST  \
+bash ~/.acme.sh/acme.sh acme.sh  --installcert  -d $HOST  \
         --cert-file /root/server.cert.pem \
         --key-file   /root/server.pem \
         --fullchain-file /root/ca.cert.pem
